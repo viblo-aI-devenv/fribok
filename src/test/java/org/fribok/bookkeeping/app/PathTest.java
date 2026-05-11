80d231c69fdebc9098b611c11213930d081a940d
Victor Blomqvist <vb@viblo.se>
2026-05-11 05:58:52 +0000

    Restore Fribok copyright notices

--- src/test/java/org/fribok/bookkeeping/app/PathTest.java
+++ src/test/java/org/fribok/bookkeeping/app/PathTest.java
@@ -1,14 +1,14 @@
 /*
  * Copyright © 2010 Stefan Kangas <skangas@skangas.se>
  *
- * This file is part of Bokfri.
+ * This file is part of Fribok.
  *
- * Bokfri is free software: you can redistribute it and/or modify it
+ * Fribok is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option)
  * any later version.
  *
- * Bokfri is distributed in the hope that it will be useful, but WITHOUT
+ * Fribok is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
