7622f9e4886b793c4ac67576070dc05dd782e130
Victor Blomqvist <vb@viblo.se>
2026-05-14 21:52:19 +0000

    Format supplier payment imports with LocalDate

--- src/main/java/se/swedsoft/bookkeeping/importexport/supplierpayments/SSSupplierPaymentImporter.java
+++ src/main/java/se/swedsoft/bookkeeping/importexport/supplierpayments/SSSupplierPaymentImporter.java
@@ -10,14 +10,14 @@ import se.swedsoft.bookkeeping.gui.util.SSBundle;
 import se.swedsoft.bookkeeping.importexport.supplierpayments.poster.*;
 import se.swedsoft.bookkeeping.importexport.supplierpayments.util.LBinLine;
 import se.swedsoft.bookkeeping.importexport.util.SSImportException;
-import se.swedsoft.bookkeeping.util.SSDateUtil;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.DateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.LinkedList;
 import java.util.List;
 import org.slf4j.Logger;
@@ -81,13 +81,12 @@ public class SSSupplierPaymentImporter {    private static final Logger LOG = Lo
 
                 LocalDate iPaymentDate = iPostTK11.getPaymentDate();
 
-                DateFormat iFormat = DateFormat.getDateInstance(DateFormat.SHORT);
+                DateTimeFormatter iFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
 
                 iOutpayment = new SSOutpayment();
                 iOutpayment.setText("Leverantörsbetalning");
                 iOutpayment.setLocalDate(iPaymentDate);
-                iOutpayment.setText(
-                        "Leverantörsbetalning " + iFormat.format(SSDateUtil.toDate(iPaymentDate)));
+                iOutpayment.setText("Leverantörsbetalning " + iPaymentDate.format(iFormat));
 
                 iOutpayments.add(iOutpayment);
             }
